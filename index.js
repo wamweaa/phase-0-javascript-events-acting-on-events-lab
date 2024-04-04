a3e819a59ad07dfab526b483932266d563533cb7
wamwea <patokinya12@gmail.com>
2024-04-04 12:37:43 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4"
+function moveDodgerLeft() {
+    const leftpx = dodger.style.left.replace("px", "")
+    const left = parseInt(leftpx, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+function moveDodgerRight() {
+    const leftpx = dodger.style.left.replace("px", "")
+    const left = parseInt(leftpx, 10);
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
